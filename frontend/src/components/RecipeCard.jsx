9999a27fffff34b60d3fd15d1e82e1d4412755f1
Sunggin <sungginkim528@gmail.com>
2025-09-15 00:35:33 +0900

    fixed favorite button

--- frontend/src/components/RecipeCard.jsx
+++ frontend/src/components/RecipeCard.jsx
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { CookingPot } from "lucide-react";
 import { ChartNoAxesColumnIncreasing } from "lucide-react";
 import { Timer } from "lucide-react";
 import { Bookmark } from "lucide-react";
 import { Soup } from "lucide-react";
+import { useRecipeStore } from "@/stores/recipeStore";
 
 const RecipeCard = ({ recipe }) => {
+  const favorites = useRecipeStore((state) => state.favorites);
+  const toggleFavorites = useRecipeStore((state) => state.toggleFavorites); 
+  const saved = favorites.some((fav) => fav.id === recipe.id);
+  const [save, setSave] = useState(false);
+  const handleSave = () => {
+    toggleFavorites(recipe);
+    setSave(!save)
+  }
+
   return (
     <div className="h-[170px] bg-white rounded-xl flex flex-col justify-between p-3">
       <div className="flex flex-col gap-2">
@@ -13,11 +23,20 @@ const RecipeCard = ({ recipe }) => {
           <div className="size-[67px] bg-[#FFDED1] rounded-3xl flex justify-center items-center">
             {<Soup color="#FF7518" className="w-[60%] h-[60%]" />}
           </div>
-          <button className="w-[30px] h-[30px] bg-white rounded-full flex justify-center items-center cursor-pointer">
-            {<Bookmark color="none" fill="gray" className="size-5" />}
+          <button
+            onClick={handleSave}
+            className={`${!saved ? "bg-white" : "bg-orange-custom"} w-[30px] h-[30px] rounded-full flex justify-center items-center cursor-pointer`}
+          >
+            {!saved ? (
+              <Bookmark color="none" fill="gray" className="size-5" />
+            ) : (
+              <Bookmark color="none" fill="white" className="size-5" />
+            )}
           </button>
         </div>
-        <p className="text-[20px] font-bold font-nunito truncate w-full">{recipe.title}</p>
+        <p className="text-[20px] font-bold font-nunito truncate w-full">
+          {recipe.title}
+        </p>
       </div>
       <div className="flex gap-5 text-[10px] text-gray-500 font-semibold h-fit w-fit">
         <p className="flex ">
--- frontend/src/pages/Favorites/FavoritesPage.jsx
+++ frontend/src/pages/Favorites/FavoritesPage.jsx
@@ -1,81 +1,11 @@
 import React from "react";
 import { Plus } from "lucide-react";
 import RecipeCard from "@/components/RecipeCard";
+import { useRecipeStore } from "@/stores/recipeStore";
 
-const recipes = [
-  {
-    name: "Spaghetti Carbonara",
-    favorite: true,
-    category: "Main Course",
-    difficulty: "Medium",
-    time: 30,
-  },
-  {
-    name: "Chocolate Chip Cookies",
-    favorite: false,
-    category: "Dessert",
-    difficulty: "Easy",
-    time: 25,
-  },
-  {
-    name: "Caesar Salad",
-    favorite: false,
-    category: "Salad",
-    difficulty: "Easy",
-    time: 15,
-  },
-  {
-    name: "Beef Stir-Fry",
-    favorite: true,
-    category: "Main Course",
-    difficulty: "Medium",
-    time: 20,
-  },
-  {
-    name: "Banana Pancakes",
-    favorite: true,
-    category: "Breakfast",
-    difficulty: "Easy",
-    time: 15,
-  },
-  {
-    name: "Chicken Curry",
-    favorite: false,
-    category: "Main Course",
-    difficulty: "Hard",
-    time: 45,
-  },
-  {
-    name: "Fruit Salad",
-    favorite: true,
-    category: "Dessert",
-    difficulty: "Easy",
-    time: 10,
-  },
-  {
-    name: "Grilled Cheese Sandwich",
-    favorite: false,
-    category: "Snack",
-    difficulty: "Easy",
-    time: 10,
-  },
-  {
-    name: "Grilled Cheese Sandwich",
-    favorite: false,
-    category: "Snack",
-    difficulty: "Easy",
-    time: 10,
-  },
-  {
-    name: "Grilled Cheese Sandwich",
-    favorite: false,
-    category: "Snack",
-    difficulty: "Easy",
-    time: 10,
-  },
-];
 
 const FavoritesPage = () => {
+  const favorites = useRecipeStore((state) => state.favorites)
   return (
     <div className="bg-[#F5F6FA] w-full min-h-screen md:p-8 p-3 relative">
       <h1 className="lg:text-3xl md:text-2xl text-lg font-semibold font-nunito">
@@ -83,8 +13,8 @@ const FavoritesPage = () => {
       </h1>
       <hr />
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 pt-4">
-        {recipes.map((recipe) => (
-          <RecipeCard recipe={recipe} />
+        {favorites.map((recipe) => (
+          <RecipeCard key={recipe.id} recipe={recipe} />
         ))}
       </div>
     </div>

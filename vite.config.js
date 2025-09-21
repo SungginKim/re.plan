73e18b3f6618c148e8c3d39deb4a9a88a26bdbcf
Sunggin <sungginkim528@gmail.com>
2025-09-21 13:58:23 +0900

    backend setup

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,16 @@ import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
 
 export default defineConfig({
+  root: path.resolve(__dirname, 'frontend'),  // frontend folder
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
-
+      '@': path.resolve(__dirname, 'frontend/src'),  // src inside frontend
       react: path.resolve(__dirname, 'node_modules/react'),
       'react-dom': path.resolve(__dirname, 'node_modules/react-dom'),
     },
   },
+  server: {
+    port: 5173
+  }
 });
